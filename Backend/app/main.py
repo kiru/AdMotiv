791f9e75ba8ba9d3dc6416b9e52553dee53f2cf1
Martin Stypinski <mstypinski@gmail.com>
2020-11-27 23:01:39 +0100

    get image skeleton

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,29 @@
-from fastapi import FastAPI
+import io
+
+from PIL import Image, ImageDraw
+from fastapi import FastAPI, status
+from starlette.responses import StreamingResponse
+
+from services.image_helper import create_image
 
 app = FastAPI()
+
+@app.post("/ad_replacement", status_code=status.HTTP_200_OK)
+async def get_add_replacement():
+    pass
+
+@app.get("/get_image", status_code=status.HTTP_200_OK)
+async def get_image(content: str):
+
+    img = create_image(content)
+    output = io.BytesIO()
+    img.save(output, format="png")
+    output.seek(0)
+    img.close()
+
+    return StreamingResponse(output, media_type="image/png")
+
+
 
 
 @app.get("/")
---
+++ Backend/app/services/image_helper.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageDraw
+
+
+def create_image(content: str):
+    img = Image.new('RGB', (100, 30), color = (73, 109, 137))
+    d = ImageDraw.Draw(img)
+    d.text((10,10), str(content), fill=(255,255,0))
+    return img
+
