cdd7f67efb7177d7b815f1041b58918f2e112336
Martin Stypinski <mstypinski@gmail.com>
2020-11-28 12:01:22 +0100

    added some shananigans

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -7,17 +7,16 @@ from services.image_helper import create_image
 
 app = FastAPI()
 
-@app.post("/get_website_content", status_code=status.HTTP_200_OK)
-def get_content():
-    pass
+
 
 @app.post("/ad_replacement", status_code=status.HTTP_200_OK)
-async def get_add_replacement():
+async def get_add_replacement(content: str):
     pass
+
+
 
 @app.post("/get_image", status_code=status.HTTP_200_OK)
 async def get_image(content: str, x_size: int, y_size: int):
-
     img = create_image(content, x_size, y_size)
     output = io.BytesIO()
     img.save(output, format="png")
@@ -25,6 +24,7 @@ async def get_image(content: str, x_size: int, y_size: int):
     img.close()
 
     return StreamingResponse(output, media_type="image/png")
+
 
 @app.get("/")
 async def root():
--- Backend/app/services/image_helper.py
+++ Backend/app/services/image_helper.py
@@ -1,10 +1,17 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 
 def create_image(content: str, x_size: int, y_size: int):
     img = Image.new('RGB', (x_size, y_size), color = (255, 255, 255))
+    font = ImageFont.truetype("arial.ttf", 14)
+
     d = ImageDraw.Draw(img)
     d.rectangle([0, 0, x_size, y_size], outline=(0,0,0), width=5)
-    d.multiline_text((10,10), content, fill=(0,0,0))
+
+    w, h = d.textsize(content, font=font)
+    h += int(h * 0.21)
+    d.text(((x_size - w) / 2, (y_size - h) / 2), text=content, fill='white', font=font)
+
+    #d.multiline_text((10,10), content, fill=(0,0,0))
     return img
 
