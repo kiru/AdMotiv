6203f673051b1fa6157c140bd7d252eb9ab00883
Florian Cassayre <florian.cassayre@gmail.com>
2020-11-28 16:18:55 +0100

    fix the requirements and change the path of the model

--- Backend/app/services/nlp_few_shot_classifier.py
+++ Backend/app/services/nlp_few_shot_classifier.py
@@ -1,10 +1,10 @@
 from flair.data import Sentence
 from flair.models.text_classification_model import TARSClassifier
 
-# Download the following file, and set its path down below
+# Download the following file, and put it in `resources` (btw it will be ignored by .gitignore)
 # TODO find a better way to do it
 # https://nlp.informatik.hu-berlin.de/resources/models/tars-base/tars-base.pt
-tars = TARSClassifier.load('tars-base.pt')
+tars = TARSClassifier.load('app/resources/tars-base.pt')
 
 # The classes: a single keyword that should be self-explanatory
 # Additionally it's possible to give a few examples
